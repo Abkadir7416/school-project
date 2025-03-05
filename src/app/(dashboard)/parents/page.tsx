1f44d1c6610382e7f0419889a12e3339c41eb994
Abdul <abdulk0749@gmail.com>
2025-03-05 07:46:13 +0530

    initial commit

---
+++ src/app/(dashboard)/parents/page.tsx
@@ -0,0 +1,24 @@
+// import Announcements from '@/components/Announcement'
+// import BigCalendar from '@/components/BigCalendar'
+// import React from 'react'
+
+// const ParentPage = () => {
+//   return (
+//     <div className="flex-1 p-4 flex flex-col gap-4 xl:flex-row">
+//       {/* LEFT  */}
+//       <div className="w-full xl:w-2/3">
+//         <div className="h-full bg-white p-4 rounded-md">
+//           <h1 className='text-xl font-semibold'>Schedule (John Doe)</h1>
+//           <BigCalendar />
+//         </div>
+//       </div>
+
+//       {/* RIGHT */}
+//       <div className="w-full lg:w-1/3">
+//         <Announcements />
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default ParentPage
---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      colors: {
+        lamaSky: "#C3EBFA",
+        lamaSkyLight: "#EDF9FD",
+        lamaPurple: "#CFCEFF",
+        lamaPurpleLight: "#F1F0FF",
+        lamaYellow: "#FAE27C",
+        lamaYellowLight: "#FEFCE8",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;

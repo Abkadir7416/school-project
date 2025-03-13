a70e4d535126a7901d002a3a901698b63279bd75
Abdul <abdulk0749@gmail.com>
2025-03-13 16:00:51 +0530

    added all remaing pages

--- src/app/(dashboard)/list/parents/page.tsx
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -2,7 +2,7 @@ import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { parentsData, role } from "../../../../lib/data"
+import { parentsData, role } from '../../../../../lib/data'
 import Image from "next/image";
 
 type Parent = {
